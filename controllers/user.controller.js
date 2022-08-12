ed40632edf188612994f4de66e035c64a0bb51ac
Emanuel Arroyo <ema201217@gmail.com>
2022-08-12 19:11:58 -0300

    add multer implementation

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,29 +1,20 @@
 const fs = require('fs')
 const path = require('path')
-const { title } = require('process')
 
-const getUsers = () =>
-  JSON.parse(
+const getUsers = () => {
+  return JSON.parse(
     fs.readFileSync(path.join(__dirname, '../data/users.json'), 'utf-8'),
-  )
-
-const storeUser = (user) => {
-  const users = getUsers()
-  const usersMoreOne = [...users, user]
-  fs.writeFileSync(
-    path.join(__dirname, '../data/users.json'),
-    JSON.stringify(usersMoreOne, null, 3),
-    'utf-8',
   )
 }
 
-const updateUser = (users) => {
+const writeUsers = (users) => {
   fs.writeFileSync(
     path.join(__dirname, '../data/users.json'),
     JSON.stringify(users, null, 3),
     'utf-8',
   )
 }
+
 module.exports = {
   usersList: (req, res) => {
     const users = getUsers()
@@ -39,12 +30,13 @@ module.exports = {
         user.last_name.toLowerCase().includes(keyword.toLowerCase()) ||
         user.first_name.toLowerCase().includes(keyword.toLowerCase()),
     )
-    res.render('usersList', { users: usersFind, title: 'User Find' })
+    return res.render('usersList', { users: usersFind, title: 'User Find' })
   },
 
   userAdd: (req, res) => {
     return res.render('userAdd')
   },
+
   userStore: (req, res) => {
     const { first_name, last_name, email, password } = req.body
     const users = getUsers()
@@ -60,8 +52,9 @@ module.exports = {
         'https://aaahockey.org/wp-content/uploads/2017/06/default-avatar-300x300.png',
       address: '',
     }
-    storeUser(newUser)
-    res.redirect(`/#${newId}`)
+    const usersMoreOne = [...users, newUser]
+    writeUsers(usersMoreOne)
+    return res.redirect(`/#${newId}`)
   },
 
   userEdit: (req, res) => {
@@ -72,27 +65,42 @@ module.exports = {
   },
 
   userUpdated: (req, res) => {
-    const userId = +req.params.id
     const users = getUsers()
+    const userId = +req.params.id
+    const {
+      first_name,
+      last_name,
+      email,
+      gender,
+      password,
+      avatarText,
+      address,
+    } = req.body
+
     const usersUpdated = users.map((user) => {
       if (user.id === userId) {
         return {
           ...user,
-          ...req.body,
+          first_name: first_name.trim(),
+          last_name: last_name.trim(),
+          gender: gender.trim(),
+          address: address.trim(),
+          avatar: req.file?.filename || avatarText?.trim() || user.avatar,
+          /* Si en el input file existe guarda eso sino existe coloca el texto del input avatar tipo texto sino existe coloca lo que ya tenia el usuario en la base de datos */
         }
       } else {
         return user
       }
     })
-    updateUser(usersUpdated)
-    res.redirect(`/#${userId}`)
+    writeUsers(usersUpdated)
+    return res.redirect(`/#${userId}`)
   },
 
   userDelete: (req, res) => {
     const userId = +req.params.id
     const users = getUsers()
     const usersMinorOne = users.filter((user) => user.id !== userId)
-    updateUser(usersMinorOne)
+    writeUsers(usersMinorOne)
     return res.redirect('/')
   },
 }
--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,25 @@
-var express = require('express');
-const { usersList, userEdit, userAdd, userStore, userUpdated, userSearch, userDelete } = require('../controllers/user.controller');
-var router = express.Router();
+var express = require('express')
+const upload = require('../middlewares/uploadFileAvatar')
+const {
+  usersList,
+  userEdit,
+  userAdd,
+  userStore,
+  userUpdated,
+  userSearch,
+  userDelete,
+} = require('../controllers/user.controller')
+var router = express.Router()
 
 /* GET users listing. */
-router.get('/', usersList);
-router.get('/search', userSearch);
-router.get('/add', userAdd);
-router.post('/store',userStore)
+router.get('/', usersList)
+router.get('/search', userSearch)
+router.get('/add', userAdd)
+router.post('/store', userStore)
 
-router.get('/edit/:id', userEdit);
-router.put('/update/:id', userUpdated);
+router.get('/edit/:id', userEdit)
+router.put('/update/:id', upload.single('avatar'), userUpdated)
 
-router.delete('/delete/:id', userDelete);
+router.delete('/delete/:id', userDelete)
 
-
-module.exports = router;
+module.exports = router
